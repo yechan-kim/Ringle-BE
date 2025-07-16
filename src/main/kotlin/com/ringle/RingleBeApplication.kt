1dfc980d204fc889a34741b10e1b10fa68062916
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2025-07-17 01:19:37 +0900

    Initial Commit

---
+++ src/main/kotlin/com/ringle/RingleBeApplication.kt
@@ -0,0 +1,11 @@
+package com.ringle
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RingleBeApplication
+
+fun main(args: Array<String>) {
+    runApplication<RingleBeApplication>(*args)
+}
